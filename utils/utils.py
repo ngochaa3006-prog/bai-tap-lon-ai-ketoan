2b863aee7052290bbc173a89071700673e690570
Ngoc Ha <Ngochaa3006@gmail.com>
2026-06-06 11:20:16 +0700

    sửa code lặp lại

--- utils/utils.py
+++ utils/utils.py
@@ -34,4 +34,3 @@ def apply_pca_algorithm(scaled_data, n_components=2):
     explained_variance = pca.explained_variance_ratio_
     
     return pca_result, explained_variance, pca
-    return pca_result, explained_variance, pca
